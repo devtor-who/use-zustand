aedaa4ceb5a4332fbf66d7270a3b760dac575f64
dinnertime <dinnertime.dev@gmail.com>
2024-01-10 09:10:15 +0900

    subscribeWithSelector 사용법 추가

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,15 +1,27 @@
-import { create } from "zustand";
+import { create } from 'zustand';
+import { subscribeWithSelector } from 'zustand/middleware';
 
 type BearState = {
   bears: number;
+};
+
+type BearAction = {
   increase: (by: number) => void;
   getBears: () => number;
 };
 
-export const useBearStore = create<BearState>((set, get) => ({
+export const useBearStore = create<BearState & BearAction>((set, get) => ({
   bears: 0,
   getBears: () => get().bears,
   increase: (by: number) => set((state) => ({ bears: state.bears + by })),
 }));
 
-export const unsubLogStore = useBearStore.subscribe(console.log);
+export const useBearStore2 = create<BearState & BearAction>()(
+  subscribeWithSelector((set, get) => ({
+    bears: 2,
+    getBears: () => get().bears,
+    increase: (by: number) => set((state) => ({ bears: state.bears + by })),
+  })),
+);
+
+export const unsubLogStore = useBearStore2.subscribe((state) => state.bears, console.log);
